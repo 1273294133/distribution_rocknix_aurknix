edc52d8f4f1b578b42882793d98d03d269207846
kk <405437445@qq.com>
2026-09-20 16:59:58 +0800

    fix: batch replace remaining sourceforge sources (net-snmp/smartmontools/minidlna/argtable2/hd-idle/iat)

--- packages/addons/addon-depends/system-tools-depends/hd-idle/package.mk
+++ packages/addons/addon-depends/system-tools-depends/hd-idle/package.mk
@@ -3,10 +3,11 @@
 
 PKG_NAME="hd-idle"
 PKG_VERSION="1.05"
-PKG_SHA256="4efefe79d145b50e055582730d9d685e485da3df3dad90fef030036d52aa3a0c"
+PKG_SHA256="a50c745bcee6fadb1ea43d78c484588e4e967b445b54e712b40943c2f3235dde"
 PKG_LICENSE="GPL"
 PKG_SITE="http://hd-idle.sourceforge.net/"
-PKG_URL="http://downloads.sourceforge.net/project/hd-idle/${PKG_NAME}-${PKG_VERSION}.tgz"
+PKG_SOURCE_DIR="hd-idle"
+PKG_URL="https://deb.debian.org/debian/pool/main/h/hd-idle/hd-idle_1.05+ds.orig.tar.xz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A utility for spinning-down external disks after a period of idle time."
 PKG_BUILD_FLAGS="-sysroot"
